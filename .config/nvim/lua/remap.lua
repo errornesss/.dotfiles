248f54b18b2223a627cef10a87296fa7da546302
errornesss <errorisonlytemporary@proton.me>
2024-01-15 14:45:50 +1100

    vim tmux navi still broken ;-;

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -6,7 +6,7 @@ local map = vim.keymap.set
 map('n', '<leader><leader>', vim.cmd.so)
 
 -- vim tmux navi
-map('n', '<c-y', '<cmd>TmuxNavigateLeft<cr><esc>')
-map('n', '<c-n', '<cmd>TmuxNavigateDown<cr><esc>')
-map('n', '<c-e', '<cmd>TmuxNavigateUp<cr><esc>')
-map('n', '<c-o', '<cmd>TmuxNavigateRight<cr><esc>')
+map('n', '<c-y>', '<cmd>TmuxNavigateLeft<cr><esc>')
+map('n', '<c-n>', '<cmd>TmuxNavigateDown<cr><esc>')
+map('n', '<c-e>', '<cmd>TmuxNavigateUp<cr><esc>')
+map('n', '<c-o>', '<cmd>TmuxNavigateRight<cr><esc>')

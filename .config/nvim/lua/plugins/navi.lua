9707d9585a9c33bd7d9ab939957e7aed4345316c
errornesss <errorisonlytemporary@proton.me>
2024-01-15 15:15:07 +1100

    navi somewhat fixed? doesnt work while in nvim-tree

--- .config/nvim/lua/plugins/navi.lua
+++ .config/nvim/lua/plugins/navi.lua
@@ -1,3 +1,17 @@
 return {
   'christoomey/vim-tmux-navigator',
+  --[[
+  cmd = {
+    'TmuxNavigateLeft',
+    'TmuxNavigateDown',
+    'TmuxNavigateUp',
+    'TmuxNavigateRight',
+  },
+  keys = {
+    {'<c-y>', '<cmd><C-U>TmuxNavigateLeft<cr>'},
+    {'<c-n>', '<cmd><C-U>TmuxNavigateDown<cr>'},
+    {'<c-e>', '<cmd><C-U>TmuxNavigateUp<cr>'},
+    {'<c-o>', '<cmd><C-U>TmuxNavigateRight<cr>'},
+  },
+  ]]--
 }
--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,12 +1,13 @@
 vim.g.mapleader = ' '
 
 local map = vim.keymap.set
+local cmd = vim.cmd
 
 -- map('n', '<leader>en', vim.cmd.Ex)
-map('n', '<leader><leader>', vim.cmd.so)
+map('n', '<leader><leader>', cmd.so)
 
 -- vim tmux navi
-map('n', '<c-y>', '<cmd>TmuxNavigateLeft<cr><esc>')
-map('n', '<c-n>', '<cmd>TmuxNavigateDown<cr><esc>')
-map('n', '<c-e>', '<cmd>TmuxNavigateUp<cr><esc>')
-map('n', '<c-o>', '<cmd>TmuxNavigateRight<cr><esc>')
+map('n', '<c-y>', cmd.TmuxNavigateLeft)
+map('n', '<c-n>', cmd.TmuxNavigateDown)
+map('n', '<c-e>', cmd.TmuxNavigateUp)
+map('n', '<c-o>', cmd.TmuxNavigateRight)

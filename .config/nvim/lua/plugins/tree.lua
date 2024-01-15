36cb95d5b625a9b637f0a62d1d305f520035af6c
errornesss <errorisonlytemporary@proton.me>
2024-01-15 20:55:11 +1100

    super scuffed nvim tree mappings

--- .config/nvim/lua/plugins/tree.lua
+++ .config/nvim/lua/plugins/tree.lua
@@ -3,22 +3,40 @@ return {
   dependencies = {
     'nvim-tree/nvim-web-devicons',
   },
+  lazy = false,
   config = function()
     local function my_on_attach(bufnr)
       local api = require('nvim-tree.api')
 
+      local function opts(desc)
+        return { desc = 'nvim-tree: ' .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
+      end
+
       -- mappings
-      api.config.mappings.default_on_attach(bufnr)
-      local map = vim.keymap.set
-      map('n', '<leader>en', api.tree.toggle) --, opts('Toggle'))
+      -- local map = vim.keymap.set
+      local function map(x, y, z)
+        vim.keymap.set('n', x, y, z)
+      end
+      -- map('', api., opts(''))
+      map('<leader>en', api.tree.toggle)
+      map('<Enter>', api.node.open.edit, opts('Open'))
+      map('a', api.fs.create, opts('Create file/directory'))
+      map('d', api.fs.remove, opts('Delete'))
+      map('D', api.fs.trash, opts('Trash'))
+      map('q', api.tree.close, opts('Close'))
+      map('R', api.fs.rename, opts('Rename'))
+      map('r', api.fs.rename_basename, opts('Rename: Basename'))
     end
 
     require('nvim-tree').setup({
       on_attach = my_on_attach,
+      hijack_cursor = true,
+      -- disable_netrw = true,
+      hijack_netrw = true,
       view = {
         cursorline = true,
         side = 'right',
-        relativenumber = true,
+        -- relativenumber = true,
         width = 40,
       },
       actions = {

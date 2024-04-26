b15be2648f40081671bfac97328a331b017006b6
errornesss <errorisonlytemporary@proton.me>
2024-04-26 09:52:22 +0000

    fixed tmux navi

--- .config/nvim/lua/plugins/navi.lua
+++ .config/nvim/lua/plugins/navi.lua
@@ -1,17 +1,4 @@
 return {
   'christoomey/vim-tmux-navigator',
-  --[[
-  cmd = {
-    'TmuxNavigateLeft',
-    'TmuxNavigateDown',
-    'TmuxNavigateUp',
-    'TmuxNavigateRight',
-  },
-  keys = {
-    {'<c-y>', '<cmd><C-U>TmuxNavigateLeft<cr>'},
-    {'<c-n>', '<cmd><C-U>TmuxNavigateDown<cr>'},
-    {'<c-e>', '<cmd><C-U>TmuxNavigateUp<cr>'},
-    {'<c-o>', '<cmd><C-U>TmuxNavigateRight<cr>'},
-  },
-  ]]--
+  event = { "VimEnter", },
 }
--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -9,7 +9,7 @@ map('n', '<leader><leader>', cmd.so)
 map ('n', 'ZW', cmd.write)
 
 -- vim tmux navi
-map('n', '<c-y>', cmd.TmuxNavigateLeft)
+--[[ map('n', '<c-y>', cmd.TmuxNavigateLeft)
 map('n', '<c-n>', cmd.TmuxNavigateDown)
 map('n', '<c-e>', cmd.TmuxNavigateUp)
-map('n', '<c-o>', cmd.TmuxNavigateRight)
+map('n', '<c-o>', cmd.TmuxNavigateRight) ]]--

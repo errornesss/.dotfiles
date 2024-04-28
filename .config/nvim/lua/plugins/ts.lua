8da856b5aab4cc0fc54d6e7db66ab38ae1621331
errornesss <errorisonlytemporary@proton.me>
2024-04-28 19:54:22 +0000

    nvim-treesitter-textobjects

--- .config/nvim/lua/plugins/ts.lua
+++ .config/nvim/lua/plugins/ts.lua
@@ -5,12 +5,24 @@ return {
   },
   build = ":TSUpdate",
   config = function()
+    local unruly_textobjects = require("unruly-worker.external.textobjects")
     require("nvim-treesitter.configs").setup({
       ensure_installed = {"lua", },
       sync_install = false,
       auto_install = true,
       highlight = {
         enable = true,
+      },
+      textobjects = {
+        select = {
+          keymaps = unruly_textobjects.select_keymaps,
+        },
+        move = {
+          goto_next_start = unruly_textobjects.move_goto_next_start,
+          goto_previous_start = unruly_textobjects.move_goto_previous_start,
+          goto_next_end = unruly_textobjects.move_goto_next_end,
+          goto_previous_end = unruly_textobjects.move_goto_previous_end,
+        },
       },
     })
   end,

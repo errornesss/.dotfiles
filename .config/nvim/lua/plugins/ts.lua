fb370eaa2b0e4a639ba171a69b08852008a96763
errornesss <errorisonlytemporary@proton.me>
2024-04-29 08:18:25 +0000

    remove ts-textobjects

--- .config/nvim/lua/plugins/ts.lua
+++ .config/nvim/lua/plugins/ts.lua
@@ -9,17 +9,6 @@ return {
       highlight = {
         enable = true,
       },
-      textobjects = {
-        select = {
-          keymaps = unruly_textobjects.select_keymaps,
-        },
-        move = {
-          goto_next_start = unruly_textobjects.move_goto_next_start,
-          goto_previous_start = unruly_textobjects.move_goto_previous_start,
-          goto_next_end = unruly_textobjects.move_goto_next_end,
-          goto_previous_end = unruly_textobjects.move_goto_previous_end,
-        },
-      },
     })
   end,
 }

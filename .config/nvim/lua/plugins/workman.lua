4d7894485150acea0922bb40479e4b39d0d669f9
errornesss <errorisonlytemporary@proton.me>
2024-04-30 19:43:03 +0000

    comment no longer a dependency

--- .config/nvim/lua/plugins/workman.lua
+++ .config/nvim/lua/plugins/workman.lua
@@ -1,14 +1,7 @@
 return {
   "slugbyte/unruly-worker",
-  dependencies = {
-    {
-      "numtostr/comment.nvim",
-      event = { "BufReadPre", "BufNewFile" },
-      opts = { sticky = false, },
-    },
-  },
   opts = {
-    skip_list = { "Z", "C-z", "@", "#", "$", "^", "C-h"},
+    skip_list = { "Z", "C-h", },
     booster = {
       easy_swap = true,
       plugin_comment = true,

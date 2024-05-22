686423229613adeb0cb052d6359fd47a824856e5
errornesss <errorisonlytemporary@proton.me>
2024-05-22 21:14:57 +1000

    no clue honestly

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -15,7 +15,7 @@ static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_main[]        = "#cfcfcf";
-static const char col_hid[]         = "#595959";
+static const char col_hid[]         = "#808080";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },

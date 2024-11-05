28f99eef8057504217d3949c12852e7d618d3169
errornesss <errorisonlytemporary@proton.me>
2024-11-05 15:00:57 +1100

    added build bind in neovim

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -16,3 +16,5 @@ map("n", "<C-y>", function() require("Navigator").left() end)
 map("n", "<C-n>", function() require("Navigator").down() end)
 map("n", "<C-e>", function() require("Navigator").up() end)
 map("n", "<C-o>", function() require("Navigator").right() end)
+
+map("n", "<C-b>", ":!./build.sh<CR>")

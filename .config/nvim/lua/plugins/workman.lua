54603abd9a3ee076037f7c441e3ba65132ddf4e1
errornesss <errorisonlytemporary@proton.me>
2025-02-28 16:35:43 +1100

    added telescope

--- .config/nvim/lua/plugins/workman.lua
+++ .config/nvim/lua/plugins/workman.lua
@@ -3,7 +3,9 @@ return {
     opts = {
         skip_list = { "z", "Z", "=", },
         booster = {
+            easy_swap = true,
             plugin_comment = true,
+            plugin_telescope_leader = true,
         },
     },
 }

7313feca51dfbc4d404e116beb77b99d70d940bd
errornesss <errorisonlytemporary@proton.me>
2025-02-28 16:37:21 +1100

    small stuffh'
    '

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -6,7 +6,7 @@ return {
         require("nvim-treesitter.configs").setup({
             ensure_installed = { "lua", "c", },
             sync_install = false,
-            auto_install = false,
+            auto_install = true,
             highlight = { enable = true, },
         })
     end,

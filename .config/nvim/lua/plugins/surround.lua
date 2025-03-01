65c13a51ab6b91fef3f09f133cd72faf32cc7883
errornesss <errorisonlytemporary@proton.me>
2025-03-02 09:59:43 +1100

    set surround search method

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,5 +1,8 @@
 return {
-    "echasnovs/mini.surround",
+    "echasnovski/mini.surround",
     version = "*",
-    opts = { highlight_duration = 1000, },
+    opts = {
+        highlight_duration = 1000,
+        search_method = "cover_or_next",
+    },
 }

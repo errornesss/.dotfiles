979c07806eb16aa70125113a797c94aff3b60865
errornesss <errorisonlytemporary@proton.me>
2025-03-07 18:55:45 +1100

    added deck layout

--- .config/suckless/dwm/config.h
+++ .config/suckless/dwm/config.h
@@ -45,6 +45,7 @@ static const Layout layouts[] = {
 	{ "[]=",      tile },    /* first entry is default */
 	{ "><>",      NULL },    /* no layout function means floating behavior */
 	{ "[M]",      monocle },
+  { "[D]",      deck },
 	{ NULL,       NULL },
 };
 

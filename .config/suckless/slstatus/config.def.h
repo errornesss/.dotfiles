526632717fb13dd8d5f34cf401e0cc38b21f334f
errornesss <errorisonlytemporary@proton.me>
2025-05-23 09:28:03 +1000

    deleted files

--- .config/suckless/slstatus/config.def.h
+++ .config/suckless/slstatus/config.def.h
@@ -66,9 +66,9 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function, format, argument */
-    // { run_command, " | %s", "pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}'" },
-    { alsa_master_vol, " | %s", NULL},
-    { battery_state, " | %s", "BAT1" }, // /sys/class/power_supply/<name>
-    { battery_perc, " %s%%", "BAT1" }, // /sys/class/power_supply/<name>
-	{ datetime, " |[%s]", "%y/%m/%d|%T" },
+  // { run_command, " | %s", "pactl get-sink-volume @DEFAULT_SINK@ | awk '{print $5}'" },
+  { alsa_master_vol, " | %s", NULL},
+  { battery_state, " | %s", "BAT1" }, // /sys/class/power_supply/<name>
+  { battery_perc, " %s%%", "BAT1" }, // /sys/class/power_supply/<name>
+  { datetime, " |[%s]", "%y/%m/%d|%T" },
 };

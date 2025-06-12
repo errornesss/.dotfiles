4ae175d7d5bc0ef190d51412a8f30f76d42c216c
errornesss <errorisonlytemporary@proton.me>
2025-06-12 22:08:49 +1000

    Revert "nvim - change when ouroboros loads"
    This reverts commit e1de586f9a0911525cbbde35a2245be9d30b6501.

--- .config/nvim/lua/plugins/ouroboros.lua
+++ .config/nvim/lua/plugins/ouroboros.lua
@@ -1,7 +1,6 @@
 return {
   "jakemason/ouroboros",
   dependencies = "nvim-lua/plenary.nvim",
-  event = { "BufReadPre", "BufNewFile", },
   ft = { "c", "h", },
   config = function()
     require("ouroboros").setup({})

47a8977842b6e5e51ff99e1ae170d6f3809b2ace
errornesss <errorisonlytemporary@proton.me>
2025-06-19 16:16:10 +1000

    nvim - stuff

--- .config/nvim/lua/plugins/surround.lua
+++ .config/nvim/lua/plugins/surround.lua
@@ -1,6 +1,7 @@
 return {
   "echasnovski/mini.surround",
   version = "*",
+  event = { "BufReadPre", "BufNewFile", },
   opts = {
     highlight_duration = 1000,
     search_method = "cover_or_next",

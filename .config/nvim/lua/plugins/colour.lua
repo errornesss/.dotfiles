55ac368be9dc8332f6d49cd778c6a88187c96e96
errornesss <errorisonlytemporary@proton.me>
2025-06-23 22:35:28 +1000

    Revert "monochrome"
    This reverts commit 81b6b50557b8eccf97212692a9af22d6c1774573.

--- .config/nvim/lua/plugins/colour.lua
+++ .config/nvim/lua/plugins/colour.lua
@@ -1,27 +1,12 @@
 return {
-  "amedoeyes/eyes.nvim",
-  lazy = false,
-  priority = 1000,
-  config = function()
-    require("eyes").setup({
-      transparent = true,
-      extend = {
-        palette = {
-          hex00 = "#333333",
-          hex01 = "#424242",
-          hex02 = "#515151",
-          hex03 = "#606060",
-          hex04 = "#707070",
-          hex05 = "#7f7f7f",
-          hex06 = "#8e8e8e",
-          hex07 = "#9e9e9e",
-          hex08 = "#adadad",
-          hex09 = "#bcbcbc",
-          hex10 = "#cccccc",
-        },
-      },
-    })
+    "edeneast/nightfox.nvim",
+    config = function()
+        require("nightfox").setup({
+            options = {
+                transparent = true,
+            },
+        })
 
-    vim.cmd("colorscheme eyes")
-  end,
+        vim.cmd("colorscheme nightfox")
+    end,
 }
--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,38 +2,10 @@ return {
   "nvim-lualine/lualine.nvim",
   dependencies = "nvim-tree/nvim-web-devicons",
   config = function()
-    local c = {
-      hex00 = "#262626",
-      hex01 = "#383838",
-      hex02 = "#494949",
-      hex03 = "#5b5b5b",
-      hex04 = "#6d6d6d",
-      hex05 = "#7f7f7f",
-      hex06 = "#919191",
-      hex07 = "#a3a3a3",
-      hex08 = "#b5b5b5",
-      hex09 = "#c6c6c6",
-      hex10 = "#d8d8d8",
-    }
-
     require("lualine").setup({
       options = {
-        theme = {
-          normal = {
-            a = { fg = c.hex10, bg = c.hex02},
-            b = { fg = c.hex10, bg = c.hex01},
-            c = { fg = c.hex10, bg = c.hex00},
-          },
-          insert = { a = { bg = c.hex03, }, z = { bg = c.hex02, }, },
-          visual = { a = { bg = c.hex04, }, z = { bg = c.hex02, }, },
-          replace = { a = { fg = c.hex01, bg = c.hex05, }, z = { bg = c.hex02, }, },
-          command = { a = { fg = c.hex01, bg = c.hex06, }, z = { bg = c.hex02, }, },
-        },
         component_separators = "|",
         section_separators = "",
-      },
-      sections = {
-        lualine_x = { "filetype", },
       },
     })
   end
